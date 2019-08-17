0566795aebd26d9534cccad835c5df920bfecaa3
cameronDz <camerondziurgot@gmail.com>
2019-08-17 11:59:30 -0400

    add article component

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import InputSection from './components/sections/input';
+import Article from './components/article';
+import * as _sampleArticle from '../../assets/data/articles/1565825763336.json';
 
 const app = () => {
   return (
@@ -8,6 +10,11 @@ const app = () => {
       <Grid xs={12} sm={1}></Grid>
       <Grid xs={12} sm={10}>
         <InputSection />
+      </Grid>
+      <Grid xs={12} sm={1}></Grid>
+      <Grid xs={12} sm={1}></Grid>
+      <Grid xs={12} sm={10}>
+        <Article {..._sampleArticle} />
       </Grid>
       <Grid xs={12} sm={1}></Grid>
     </Grid>);
---
+++ src/js/components/article/index.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+
+const article = props => {
+
+  const [author, setAuthor] = useState('');
+  const [comments, setComments] = useState([]);
+  const [createdDate, setCreatedDate] = useState('');
+  const [description, setDescription] = useState('');
+  const [publishDate, setPublishDate] = useState('');
+  const [publisher, setPublisher] = useState('');
+  const [quotes, setQuotes] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const { author, comments, createdDate, description, publishDate, publisher, quotes, tags, title, url } = props;
+    console.log('props', props);
+    setAuthor(author);
+    setComments(comments);
+    setCreatedDate(createdDate);
+    setDescription(description);
+    setPublishDate(publishDate);
+    setPublisher(publisher);
+    setQuotes(quotes);
+    setTags(tags);
+    setTitle(title);
+    setUrl(url);
+  }, []);
+
+  const renderTags = () => {
+    const includeComma = index => { return (index !== tags.length -1) ? ', ' : ''};
+    return !!tags.length
+      ? tags.map((key, index) => { return <i key={index}>{key}{includeComma(index)}</i> })
+      : <i>No associated tags.</i>;
+  };
+
+  const renderArrayConent = (array = [], identifier = '') => {
+    const bullet = <span>&#8226;</span>;
+    return !! (!!identifier && !!array.length)
+      ? array.map((key = {}, index) => { return <div key={index}>{bullet} {key[identifier]}</div> })
+      : "No content."
+  };
+
+  const renderArray = (array = [], identifier = '', title = '')  => {
+    const header = <strong style={{'font-size':'16px'}}>{title}</strong>;
+    return (
+      <React.Fragment>
+        <div>{header}</div>
+        {renderArrayConent(array, identifier)}
+      </React.Fragment>);
+  };
+
+  return (
+    <React.Fragment>
+      <div style={{'font-size':'24px'}}>{title}</div>
+      <div style={{'font-size':'12px'}}>{author}, <i>Source</i>: <a href={url}>{publisher}</a>. ({publishDate})</div>
+      <div style={{'font-size':'18px'}}>{description}</div>
+      <p>{renderArray(quotes, 'quote', "Quotes")}</p>
+      <p>{renderArray(comments, 'comment', "Comments")}</p>
+      <p><strong>Tags</strong>: {renderTags()}</p>
+      <p><strong>Resource added</strong>: <i>{createdDate}</i></p>
+    </React.Fragment>);
+};
+
+export default article;
