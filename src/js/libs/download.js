e0a91c5f24781ba4a5cf4b2ee8b98dd8391a0b6d
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-27 23:21:30 -0400

    move href value to all single line

--- src/js/libs/download.js
+++ src/js/libs/download.js
@@ -1,12 +1,11 @@
 const downloadJson = (object = '') => {
   if (!object) { return; }
   const filename = new Date().getTime() + '.json';
-  const contentType = "application/json;charset=utf-8;";
   
   // fake a click to download file
   const element = document.createElement('a');
   element.download = filename;
-  element.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(object));
+  element.href = 'data:application/json;charset=utf-8;,' + encodeURIComponent(JSON.stringify(object));
   element.target = '_blank';
   document.body.appendChild(element);
   element.click();
