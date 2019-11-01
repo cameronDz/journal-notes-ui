fd7466006ddb5ddf49824d6b87a7fee4f09907da
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-31 22:43:40 -0400

    create state action/reducers for fetching articles from s3, add to global state

--- src/js/state/rootReducer.js
+++ src/js/state/rootReducer.js
@@ -1,3 +1,4 @@
 import { combineReducers } from 'redux';
+import articlesReducer from '../components/lists/articles/state/reducer';
 
-export default combineReducers({});
+export default combineReducers({ articles: articlesReducer });
