968947abb073c242159fad32d088aeab07ee56c9
Cam Dziurgot <camerondziurgot@gmail.com>
2020-09-27 02:34:05 -0400

    set inputs to use material ui, add preview to display no matter what is in fields

--- src/js/components/sections/input/index.jsx
+++ src/js/components/sections/input/index.jsx
@@ -1,16 +1,15 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import PropType from 'prop-types';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 import Card from '../../card';
 
 import { uploadArticle } from './state/actions';
 import { downloadJson } from '../../../libs/download';
 import { generateDateString } from '../../../libs/date';
-
-const PREVIEW_CARD_TITLE = 'Click to Preview Card';
-const REMOVE_CARD_TITLE = 'Click to Remove Preview';
 
 const propTypes = { uploadArticle: PropType.func };
 const input = ({ uploadArticle }) => {
@@ -28,10 +27,6 @@ const input = ({ uploadArticle }) => {
   const [tag, setTag] = useState('');
   const [tags, setTags] = useState([]);
 
-  const [payload, setPayload] = useState('');
-  const [previewTitle, setPreviewTitle] = useState(PREVIEW_CARD_TITLE);
-  const [isPreviewing, setIsPreviewing] = useState(false);
-
   const handleClearClick = () => {
     setAuthor('');
     setDescription('');
@@ -45,34 +40,27 @@ const input = ({ uploadArticle }) => {
     setQuotes([]);
     setTag('');
     setTags([]);
-    setPayload('');
   };
 
   const handleUploadClick = () => {
-    if (payload) {
-      uploadArticle(payload);
-    }
+    const payload = generatePayload();
+    uploadArticle(payload);
   };
 
   const handleDownloadClick = () => {
-    if (payload) {
-      downloadJson(JSON.parse(payload));
-    }
+    const payload = generatePayload();
+    downloadJson(JSON.parse(payload));
   };
 
-  const handleCreateClick = () => {
+  const generatePayload = () => {
     handleAddComment();
     handleAddQuote();
     handleAddTag();
-    const obj = { author, comments, description, publishDate, publisher, quotes, tags, title, url, createdDate: generateDateString() };
-    setPayload(JSON.stringify(obj));
-  };
+    return generateCardPayload();
+  }
 
-  const handlePreviewClick = () => {
-    const previewing = !isPreviewing;
-    const title = previewing ? REMOVE_CARD_TITLE : PREVIEW_CARD_TITLE;
-    setIsPreviewing(previewing);
-    setPreviewTitle(title);
+  const generateCardPayload = () => {
+    return { author, comments, description, publishDate, publisher, quotes, tags, title, url, createdDate: generateDateString() };
   };
 
   const handleAddComment = () => {
@@ -101,92 +89,71 @@ const input = ({ uploadArticle }) => {
   const handleRemoveTag = () => { setTags([...tags.splice(0, tags.length - 1)]); };
 
   const getPreview = () => {
-    const content = (!!title && (comments.length || quotes.length))
-      ? getCard()
-      : <div>No preview availabe without a title and a comment/quote from article.</div>;
-    return !!isPreviewing && (
+    return (
       <Grid item sm={12}>
-        {content}
+        <Card articleData={generateCardPayload()} show={true} />
       </Grid>);
-  };
-
-  const getCard = () => {
-    const key = { author, comments, description, publishDate, publisher, quotes, tags, title, url };
-    return <Card articleData={key} />;
-  };
-
-  const renderArray = (array = [], identifier = '') => {
-    return array.map((key, index) => {
-      const comma = (index !== array.length - 1) ? ', ' : '';
-      const display = (identifier) ? key[identifier] : key;
-      return (<Fragment key={index}>{display}{comma}</Fragment>);
-    });
   };
 
   return (
     <Grid container spacing={0}>
       <Grid item xs={12}>
-        <h2>Create Article JSON</h2>
+        <h2>Article Review</h2>
       </Grid>
       <Grid item xs={12} sm={4}>
         <Grid item xs={12}>
-          <label forhtml="title">Title:</label><br/>
-          <input style={{ marginBottom: '8px', width: '90%' }} name="title" onChange={event => setTitle(event.target.value)} type="text" value={title} />
+          <TextField fullWidth label="Title" onChange={event => setTitle(event.target.value)} value={title}></TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="author">Author:</label><br/>
-          <input style={{ marginBottom: '8px', width: '90%' }} name="author" onChange={event => setAuthor(event.target.value)} type="text" value={author} />
+          <TextField fullWidth label="Author" onChange={event => setAuthor(event.target.value)}  value={author}></TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="publishDate">Publish Date:</label><br/>
-          <input style={{ marginBottom: '8px', width: '90%' }} name="publishDate" onChange={event => setPublishDate(event.target.value)} type="date" value={publishDate} />
+          <TextField
+            fullWidth
+            label="Publish Date"
+            onChange={event => setPublishDate(event.target.value)}
+            type="date"
+            value={publishDate}
+            InputLabelProps={{ shrink: true }}>
+          </TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="publisher">Publisher:</label><br/>
-          <input style={{ marginBottom: '8px', width: '90%' }} name="publisher" onChange={event => setPublisher(event.target.value)} type="text" value={publisher} />
+          <TextField fullWidth label="Publisher" onChange={event => setPublisher(event.target.value)}  value={publisher}></TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="url">URL:</label><br/>
-          <input style={{ marginBottom: '8px', width: '90%' }} name="url" onChange={event => setUrl(event.target.value)} type="text" value={url} />
+          <TextField fullWidth label="URL" onChange={event => setUrl(event.target.value)}  value={url}></TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="tag">Tag: {renderArray(tags)}</label><br/>
-          <input style={{ marginBottom: '4px', width: '90%' }} name="tag" onChange={event => setTag(event.target.value)} value={tag} />
-          <button style={{ marginRight: '16px' }} onClick={handleAddTag}>Add a new Tag</button>
-          <button style={{ marginBottom: '8px' }} onClick={handleRemoveTag}>Clear last Tag</button>
+          <TextField fullWidth label="Tag" onChange={event => setTag(event.target.value)}  value={tag}></TextField>
+          <Button style={{ marginRight: '16px' }} onClick={handleAddTag} disabled={!tag}>Add Tag</Button>
+          <Button onClick={handleRemoveTag} disabled={!tags.length}>Remove Tag</Button>
         </Grid>
       </Grid>
-      <Grid item xs={12} sm={8}>
+      <Grid item xs={12} sm={1}>
+        </Grid>
+      <Grid item xs={12} sm={7}>
         <Grid item xs={12}>
-          <label forhtml="description">Description:</label><br/>
-          <textarea style={{ width: '100%' }} name="description" onChange={event => setDescription(event.target.value)} value={description}></textarea>
+          <TextField fullWidth multiline label="Description" onChange={event => setDescription(event.target.value)}  value={description}></TextField>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="comments">Comments: {renderArray(comments, 'comment')}</label><br/>
-          <textarea style={{ width: '100%' }} name="comments" onChange={event => setComment(event.target.value)} value={comment}></textarea>
-          <button style={{ marginRight: '16px' }} onClick={handleAddComment}>Add a new Comment</button>
-          <button style={{ marginBottom: '8px' }} onClick={handleRemoveComment}>Remove last Comment</button>
+          <TextField fullWidth multiline label="Comments" onChange={event => setComment(event.target.value)}  value={comment}></TextField>
+          <Button style={{ marginRight: '16px' }} onClick={handleAddComment} disabled={!comment}>Add Comment</Button>
+          <Button onClick={handleRemoveComment} disabled={!comments.length}>Remove Comment</Button>
         </Grid>
         <Grid item xs={12}>
-          <label forhtml="quote">Quotes: {renderArray(quotes, 'quote')}</label><br/>
-          <textarea style={{ width: '100%' }} name="quote" onChange={event => setQuote(event.target.value)} value={quote}></textarea>
-          <button style={{ marginRight: '16px' }} onClick={handleAddQuote}>Add a new Quote</button>
-          <button style={{ marginBottom: '8px' }} onClick={handleRemoveQuote}>Clear last Quote</button>
+          <TextField fullWidth multiline label="Quotes" onChange={event => setQuote(event.target.value)}  value={quote}></TextField>
+          <Button style={{ marginRight: '16px' }} onClick={handleAddQuote} disabled={!quote}>Add Quote</Button>
+          <Button onClick={handleRemoveQuote} disabled={!quotes.length}>Remove Quote</Button>
         </Grid>
-      </Grid>
-      <Grid style={{ border: '1px solid grey', padding: '4px' }} item xs={12}>
-        <button style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleDownloadClick}>Download JSON Payload</button>
-        <button style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleUploadClick}>Upload JSON to S3</button>
-        <button style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleClearClick}>Clear Payload</button>
-        <Grid item md={3} sm={6} xs={12}></Grid>
-        <Grid item md={3} sm={6} xs={12}><button onClick={handleCreateClick}>Create JSON</button></Grid>
-        <Grid item xs={12}><p>{' Payload: ' + payload}</p></Grid>
       </Grid>
       <Grid item xs={12}>
-        <button style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handlePreviewClick}>{previewTitle}</button>
-      </Grid>
-      <Grid item xs={12}>
+        <div style={{ fontSize: '20px', marginBottom: '12px'}}>Card Preview</div>
         {getPreview()}
+      </Grid>
+      <Grid style={{ marginTop: '12px', padding: '4px' }} item xs={12}>
+        <Button variant="outlined" style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleDownloadClick}>Download form Data</Button>
+        <Button variant="outlined" style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleUploadClick}>Upload form Data to S3</Button>
+        <Button variant="outlined" style={{ marginRight: '16px', marginBottom: '8px' }} onClick={handleClearClick}>Clear Form</Button>
       </Grid>
     </Grid>);
 };
