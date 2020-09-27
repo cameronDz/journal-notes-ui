9950b23a827b29ec11e11735b137e0747fe27db2
Cam Dziurgot <camerondziurgot@gmail.com>
2020-09-26 22:38:30 -0400

    add webpack file loader

--- webpack/common.babel.js
+++ webpack/common.babel.js
@@ -15,6 +15,11 @@ module.exports = {
       use: {
         loader: 'html-loader'
       }
+    }, {
+      test: /\.(png|jpe?g|gif)$/i,
+      use: {
+        loader: 'file-loader',
+      },
     }]
   },
   output: {
