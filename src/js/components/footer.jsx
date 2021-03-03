5cfb4c18d3892430b9a482bcea1ef777ae1798d3
cameronDz <camerondziurgot@gmail.com>
2021-03-02 23:54:53 -0500

    add footer component

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { version } from '../../../package.json';
+
+const topStyles = {
+  width: '100%'
+};
+const centeredStyles = {
+  fontStyle: 'italic',
+  margin: 'auto',
+  maxWidth: '1080px',
+  textAlign: 'center'
+};
+
+const displayDate = new Date().getFullYear();
+const displayName = 'Cam Dziurgot';
+const displayVersion = 'v' + version;
+
+const footer = () => {
+  return (
+    <div style={topStyles}>
+      <div style={centeredStyles}>{displayName} &copy; {displayDate} {displayVersion}</div>
+    </div>);
+};
+
+export default footer;
