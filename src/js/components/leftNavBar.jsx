25d6ff2bb77c9db4d3504a5594d6e1f9ed501953
cameronDz <camerondziurgot@gmail.com>
2021-08-27 19:17:26 -0400

    update navbar with icon

--- src/js/components/leftNavBar.jsx
+++ src/js/components/leftNavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import classNames from "classnames";
+import { SvgIcon } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { navBarStyles } from "./styles";
 
@@ -8,7 +9,13 @@ const LeftNavBar = () => {
   const classes = useStyles();
   return (
     <Fragment>
-      <div className={classNames(classes?.navBarRoot)}></div>;
+      <div className={classNames(classes?.navBarRoot)}>
+        <div className={classNames(classes?.iconWrapper)}>
+          <SvgIcon fontSize="large">
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+          </SvgIcon>
+        </div>
+      </div>
     </Fragment>
   );
 };
