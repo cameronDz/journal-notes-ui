10e30074c1411aeda755cff1d12f8eee5a90dc5f
cameronDz <camerondziurgot@gmail.com>
2021-08-26 21:01:36 -0400

    update with styles

--- src/js/components/leftNavBar.jsx
+++ src/js/components/leftNavBar.jsx
@@ -1,7 +1,16 @@
 import React, { Fragment } from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+import { navBarStyles } from "./styles";
 
+const useStyles = makeStyles(() => navBarStyles);
 const LeftNavBar = () => {
-  return <Fragment></Fragment>;
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <div className={classNames(classes?.navBarRoot)}></div>;
+    </Fragment>
+  );
 };
 
 export default LeftNavBar;
---
+++ src/js/components/styles.js
@@ -0,0 +1,5 @@
+const navBarStyles = {
+  navBarRoot: {},
+};
+
+export { navBarStyles };
