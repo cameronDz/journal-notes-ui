b22ceb5af8beb5298f3e0b7c6590d8dbc127f9e9
cameronDz <camerondziurgot@gmail.com>
2021-08-28 18:04:43 -0400

    impl overview and details sections

--- src/js/components/article.jsx
+++ src/js/components/article.jsx
@@ -1,8 +1,7 @@
 import React, { Fragment } from "react";
 import PropType from "prop-types";
-import ArticleBulletPoints from "./articleBulletPoints";
-import ArticleSectionHeader from "./articleSectionHeader";
-import ArticleTags from "./ArticleTags";
+import ArticleDetails from "./articleDetails";
+import ArticleOverview from "./articleOverview";
 
 const propTypes = {
   author: PropType.string,
@@ -31,67 +30,20 @@ const Article = ({
   title,
   url,
 }) => {
-  const renderTopSection = () => {
-    const display = publisher || url || "";
-    const link = display && url ? <a href={url}>{display}</a> : display;
-    const sourceDisplay = link && (
-      <Fragment>
-        <i>Source</i>
-        {`: ${link}.`}
-      </Fragment>
-    );
-
-    const publishDateDisplay = publishDate || "";
-    const comma = !!author && (!!sourceDisplay || !!publishDateDisplay) && ",";
-    const authorDisplay = !!author && (
-      <Fragment>
-        {author}
-        {comma}
-      </Fragment>
-    );
-    return (
-      <Fragment>
-        <div style={{ fontSize: "24px", lineHeight: "28px" }}>{title}</div>
-        <div style={{ fontSize: "12px" }}>
-          {authorDisplay} {sourceDisplay} {publishDateDisplay}
-        </div>
-        <div style={{ fontSize: "12px" }}>
-          <strong>Resource added</strong>: <i>{createdDate}</i>
-        </div>
-        <div style={{ fontSize: "18px", lineHeight: "24px" }}>
-          {description}
-        </div>
-      </Fragment>
-    );
-  };
-
-  const renderBottomSection = () => {
-    return (
-      !!showFull && (
-        <Fragment>
-          <ArticleBulletPoints
-            keyName="comment"
-            points={comments}
-            title="Comments"
-          />
-          <ArticleBulletPoints keyName="quote" points={quotes} title="Quotes" />
-          {(!Array.isArray(comments) || comments?.length < 1) &&
-            (!Array.isArray(quotes) || quotes?.length < 1) && (
-              <Fragment>
-                <ArticleSectionHeader title={"Comments/Quotes"} />
-                <div>No Comment or Quote content to display.</div>
-              </Fragment>
-            )}
-          <ArticleTags tags={tags} />
-        </Fragment>
-      )
-    );
-  };
-
   return (
     <Fragment>
-      {renderTopSection()}
-      {renderBottomSection()}
+      <ArticleOverview
+        author={author}
+        createdDate={createdDate}
+        description={description}
+        publishDate={publishDate}
+        publisher={publisher}
+        title={title}
+        url={url}
+      />
+      {!!showFull && (
+        <ArticleDetails comments={comments} quotes={quotes} tags={tags} />
+      )}
     </Fragment>
   );
 };
