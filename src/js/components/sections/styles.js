d944288830452d693f03d7bbb97a0e9fa135cebd
cameronDz <camerondziurgot@gmail.com>
2021-08-27 21:57:06 -0400

    mv out styles

---
+++ src/js/components/sections/styles.js
@@ -0,0 +1,6 @@
+const landingStyles = {
+  headerLandingText: { alignItems: "center", justifyContent: "center" },
+  simpleLandingText: { fontSize: "14px", fontWeight: 600 },
+};
+
+export { landingStyles };
