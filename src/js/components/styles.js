de6248f36dd15d7002ed68962b72c30ad729e1b0
cameronDz <camerondziurgot@gmail.com>
2021-08-27 21:07:30 -0400

    mv styling

--- src/js/components/styles.js
+++ src/js/components/styles.js
@@ -4,12 +4,19 @@ const navBarStyles = {
   navBarRoot: {
     marginTop: globalDimensions?.appNavBarSize?.px,
   },
+};
+
+const navBarIconStyles = {
   iconWrapper: {
+    marginBottom: "12px",
     "& svg": {
       margin: "0 auto",
       width: "100%",
+      "&:hover": {
+        cursor: "pointer",
+      },
     },
   },
 };
 
-export { navBarStyles };
+export { navBarIconStyles, navBarStyles };
