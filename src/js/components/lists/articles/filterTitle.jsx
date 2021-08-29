93b2746f7062315e3779f0493ea70d2f9706f870
cameronDz <camerondziurgot@gmail.com>
2021-08-28 21:41:08 -0400

    fix import

--- src/js/components/lists/articles/filterTitle.jsx
+++ src/js/components/lists/articles/filterTitle.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import classNames from "classnames";
 import PropType from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { fitlerTitleStyles } from "./styles";
+import { filterTitleStyles } from "./styles";
 
 const propTypes = { title: PropType.string };
-const useStyles = makeStyles(() => fitlerTitleStyles);
+const useStyles = makeStyles(() => filterTitleStyles);
 const FilterTitle = ({ title }) => {
   const classes = useStyles();
   return (
