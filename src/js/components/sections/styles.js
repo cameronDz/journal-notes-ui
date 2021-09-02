331b05ec14af1eebcc69b3dd1333c3b269dbe6cd
cameronDz <camerondziurgot@gmail.com>
2021-09-01 20:13:54 -0400

    double margin lft

--- src/js/components/sections/styles.js
+++ src/js/components/sections/styles.js
@@ -23,7 +23,7 @@ const contentStyles = {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     fontSize: "24px",
     fontWeight: "700",
-    marginLeft: "24px",
+    marginLeft: "48px",
     width: "100%",
   },
 };
