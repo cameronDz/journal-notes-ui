7de484666f510b32eef48ebde2c1cdf6274870c6
cameronDz <camerondziurgot@gmail.com>
2021-09-08 04:22:40 -0400

    add route title comp

---
+++ src/js/components/sections/routeTitle.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classNames from "classnames";
+import PropType from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { routeTitleStyles } from "./styles";
+
+const propTypes = { title: PropType.string };
+const useStyles = makeStyles(() => routeTitleStyles);
+const RouteTitle = ({ title }) => {
+  const classes = useStyles();
+  return (
+    <div className={classNames(classes?.panelHeader)}>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+RouteTitle.propTypes = propTypes;
+export default RouteTitle;
