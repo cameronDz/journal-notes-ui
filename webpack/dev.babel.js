e56a5d18bb2220ef5766f5377c2a78dae7e40519
cameronDz <camerondziurgot@gmail.com>
2021-09-10 17:38:11 -0400

    update webpack config

--- webpack/dev.babel.js
+++ webpack/dev.babel.js
@@ -5,9 +5,10 @@ import common from "./common.babel";
 
 const dev = {
   devServer: {
-    contentBase: resolve(__dirname, "..", "./dist"),
+    static: {
+      publicPath: resolve(__dirname, "..", "./dist"),
+    },
     hot: true,
-    watchContentBase: true,
   },
   devtool: "inline-source-map",
   mode: "development",
