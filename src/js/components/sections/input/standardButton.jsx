e8414f3cf570fe9255e7adf9b3b06b7c8cbaf5f9
cameronDz <camerondziurgot@gmail.com>
2021-09-11 00:42:08 -0400

    add scaf for button, add possible acts

---
+++ src/js/components/sections/input/standardButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button as MuiButton } from "@material-ui/core";
+
+const StandardButton = () => {
+  return <MuiButton></MuiButton>;
+};
+
+export default StandardButton;
