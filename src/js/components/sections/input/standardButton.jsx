ea1f1d5f1b0039c07b48b0077763e721878ab616
cameronDz <camerondziurgot@gmail.com>
2021-09-11 00:52:04 -0400

    set up button with styles

--- src/js/components/sections/input/standardButton.jsx
+++ src/js/components/sections/input/standardButton.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import PropType from "prop-types";
 import { Button as MuiButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { standardButtonStyles } from "./styles";
 
-const StandardButton = () => {
-  return <MuiButton></MuiButton>;
+const propTypes = {
+  disabled: PropType.bool,
+  label: PropType.string,
+  onClick: PropType.func,
+  title: PropType.string,
+  variant: PropType.string,
 };
 
+const useStyles = makeStyles(() => standardButtonStyles);
+const StandardButton = ({
+  disabled = false,
+  label = "",
+  onClick = null,
+  title = "",
+  variant = "outlined",
+}) => {
+  const classes = useStyles();
+  const handleClick = (event) => {
+    if (typeof onClick === "function") {
+      onClick(event);
+    }
+  };
+  return (
+    <MuiButton
+      classes={classes?.standardButtonRoot}
+      disabled={disabled}
+      onClick={handleClick}
+      title={title}
+      variant={variant}
+    >
+      {label}
+    </MuiButton>
+  );
+};
+
+StandardButton.propTypes = propTypes;
 export default StandardButton;
---
+++ src/js/components/sections/input/styles.js
@@ -0,0 +1,9 @@
+const standardButtonStyles = {
+  standardButtonRoot: {
+    marginBottom: "8px",
+    marginRight: "16px",
+    width: "200px",
+  },
+};
+
+export { standardButtonStyles };
