1f42b2cdb3961b352cfddb3c1db08cc3e83d10ec
cameronDz <camerondziurgot@gmail.com>
2021-09-18 15:28:28 -0400

    rm button, add styles

---
+++ src/js/auth/components/styles.js
@@ -0,0 +1,16 @@
+const requestTokenDialogStyles = {
+  dialogContainer: {
+    width: "540px",
+    "& .MuiCircularProgress-root": {
+      display: "block",
+      height: "80px !important",
+      margin: "auto",
+      width: "80px !important",
+    },
+  },
+  dialogContentContainer: {
+    minHeight: "100px",
+  },
+};
+
+export { requestTokenDialogStyles };
