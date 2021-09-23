1c73734c67ea006f5614ff643fbe4b206bf2e19a
cameronDz <camerondziurgot@gmail.com>
2021-09-23 00:50:06 -0400

    add change handler and default value passer

--- src/js/components/sections/journalForm.jsx
+++ src/js/components/sections/journalForm.jsx
@@ -1,15 +1,38 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import PropType from "prop-types";
 import { Grid } from "@material-ui/core";
 import InputContainer from "../inputs/inputContainer";
+import { generateFormValues } from "../../libs/generateFormValues";
 
 const propTypes = {
+  defaultValues: PropType.object,
   inputs: PropType.array,
+  isDisabled: PropType.bool,
   name: PropType.string,
 };
-const JournalForm = ({ inputs, name }) => {
+const JournalForm = ({
+  defaultValues = null,
+  inputs = null,
+  isDisabled = true,
+  name = "",
+}) => {
+  const [hasSetValues, setHasSetValues] = useState(false);
+  const [values, setValues] = useState(null);
+
+  useEffect(() => {
+    if (!hasSetValues && !!defaultValues && !!inputs) {
+      const generatedValues = generateFormValues(inputs, defaultValues);
+      setValues(generatedValues);
+      setHasSetValues(true);
+    }
+  }, [defaultValues, hasSetValues, inputs]);
+
   const handleUpdate = (updateValue, updateName) => {
-    console.info("update value: ", updateValue, ", name: ", updateName);
+    if (values?.[updateName] !== undefined) {
+      setValues((prev) => {
+        return { ...prev, [updateName]: updateValue };
+      });
+    }
   };
 
   return (
@@ -21,10 +44,12 @@ const JournalForm = ({ inputs, name }) => {
               <Fragment key={input.id || index}>
                 <Grid item xs={12} sm={12} md={12}>
                   <InputContainer
+                    isDisabled={isDisabled}
                     name={input.name}
                     onUpdate={(update) => handleUpdate(update, name)}
                     title={input.title}
                     type={input.type}
+                    value={values?.[input.name]}
                   />
                 </Grid>
               </Fragment>
