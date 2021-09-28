047d781c8d443ef19038e3e7aad932c837e4b1db
cameronDz <camerondziurgot@gmail.com>
2021-09-27 21:51:58 -0400

    upate path

--- src/js/state/rootReducer.js
+++ src/js/state/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import articlesReducer from "../components/lists/articles/state/reducer";
+import articlesReducer from "../sections/articles/state/reducer";
 import authReducer from "../auth/state/reducer";
 import editorReducer from "../sections/editor/state/reducer";
 
