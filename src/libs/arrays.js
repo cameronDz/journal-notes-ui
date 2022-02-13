b49fb83ffa737dd0fceb9668d324c7b3c6f11b89
Cam Dziurgot <camerondziurgot@gmail.com>
2022-02-13 16:16:29 -0500

    update libs

---
+++ src/libs/arrays.js
@@ -0,0 +1,5 @@
+const hasItems = (arr) => {
+  return Array.isArray(arr) && arr.length > 0;
+};
+
+export { hasItems };
--- src/libs/transformer.js
+++ src/libs/transformer.js
@@ -1,8 +1,12 @@
+import { defaultUniqueArray } from "./defaults";
 import { journalTypes } from "./types";
 
 const transformValuesToCurrentVersion = (values = null) => {
   let tranformedValues;
   switch (values?.journalType) {
+    case journalTypes.ARTICLE:
+      tranformedValues = transformArticleValues(values);
+      break;
     case journalTypes.BOOK:
       tranformedValues = transformBookValues(values);
       break;
@@ -16,12 +20,18 @@ const transformBookValues = (values) => {
   const version = values._version || 0;
   let transformedValues = values;
   if (!version || version === "1.0.0") {
-    transformedValues.definitions = [
-      ...(Array.isArray(values.definitions) ? values.definitions : []),
-    ];
-    transformedValues.resources = [
-      ...(Array.isArray(values.resources) ? values.resources : []),
-    ];
+    transformedValues.definitions = defaultUniqueArray(values.definitions);
+    transformedValues.resources = defaultUniqueArray(values.resources);
+    transformedValues._version = "1.1.0";
+  }
+  return transformedValues;
+};
+
+const transformArticleValues = (values) => {
+  const version = values._version || 0;
+  let transformedValues = values;
+  if (!version || version === "1.0.0") {
+    transformedValues.definitions = defaultUniqueArray(values.definitions);
     transformedValues._version = "1.1.0";
   }
   return transformedValues;
