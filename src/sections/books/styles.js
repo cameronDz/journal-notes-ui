b4581f27e83d9fbcbb9a82ca8bd80a2c0176714a
Cam Dziurgot <camerondziurgot@gmail.com>
2022-05-01 00:47:20 -0400

    commit style js

---
+++ src/sections/books/styles.js
@@ -0,0 +1,36 @@
+const bookSectionStyles = {
+  navIconWrapper: {
+    "&$block .MuiButtonBase-root": {
+      width: "72px",
+    },
+  },
+  block: {
+    display: "block",
+  },
+  inlineBlock: {
+    display: "inline-block",
+  },
+  flex: {
+    display: "flex",
+  },
+  margin12: {
+    margin: "12px",
+  },
+  marginR12: {
+    marginRight: "12px",
+  },
+  overflowXHidden: {
+    overflowX: "hidden",
+  },
+  padding2: {
+    padding: "2px",
+  },
+  paddingB6: {
+    paddingBottom: "6px",
+  },
+  width72: {
+    width: "72px",
+  },
+};
+
+export { bookSectionStyles };
