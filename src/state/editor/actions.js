75abd1a6285640c4313ea8db9b7f240d53748f4e
Cam D <camerondziurgot@gmail.com>
2026-08-11 22:34:49 -0400

    rm cleanup filter

--- src/state/editor/actions.js
+++ src/state/editor/actions.js
@@ -54,8 +54,7 @@ const upsertIndex = (item, config = {}) => {
           throw new Error("Saving is disabled");
         }
         const currIndex = defaultUniqueArray(getState().notes?.index);
-        // eslint-disable-next-line prettier/prettier
-        const newIndex = defaultUniqueArray([...currIndex, item]).filter(i => i !== "041a65be-15a3-46f9-9d7e-35bb2dde00e8");
+        const newIndex = defaultUniqueArray([...currIndex, item]);
         const body = { list: newIndex };
         const url = `${baseApiUrl}/update/index`;
         const configuration = { ...baseConfig, ...defaultEmptyObject(config) };
